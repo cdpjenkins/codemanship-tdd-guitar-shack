26cdf7e07080e7f68447e043aaa9dfaf9c1aa82e
Chris Jenkins <cdpjenkins@gmail.com>
2026-04-15 22:34:49 +0100

    feat: Add hold when insufficient stock raises error

--- src/test/java/com/codemanship/AddItemTest.java
+++ src/test/java/com/codemanship/AddItemTest.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AddItemTest {
 
     @Test
     void temporary_hold_is_placed_when_sufficient_stock() {
-        Product product = new Product(327, 7, 0);
+        Product product = new Product(327, "Ibanez Tube Screamer", 7, 0);
         Order order = new Order();
 
         order.addItem(product, 1);
@@ -21,22 +22,34 @@ public class AddItemTest {
 
     @Test
     void item_is_added_to_order_list_when_sufficient_stock() {
-        Product product = new Product(327, 7, 0);
+        Product product = new Product(327, "Ibanez Tube Screamer", 7, 0);
         Order order = new Order();
 
         order.addItem(product, 1);
 
         assertThat(order.getItems()).containsExactly(new OrderItem(327, 1));
     }
+
+    @Test
+    void raises_error_when_insufficient_product_stock() {
+        Product product = new Product(327, "Ibanez Tube Screamer", 1, 0);
+        Order order = new Order();
+
+        assertThatThrownBy(() -> order.addItem(product, 2))
+                .isInstanceOf(IllegalStateException.class)
+            .hasMessage("Insufficient stock of Ibanez Tube Screamer. Only 1 currently available.");
+    }
 }
 
 class Product {
     private final int id;
+    private final String description;
     private final int stock;
     private int hold;
 
-    public Product(int id, int stock, int hold) {
+    public Product(int id, String description, int stock, int hold) {
         this.id = id;
+        this.description = description;
         this.stock = stock;
         this.hold = hold;
     }
@@ -52,13 +65,24 @@ class Product {
     public int getId() {
         return id;
     }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
 
 class Order {
-
     List<OrderItem> orderItems = new ArrayList<>();
 
     public void addItem(Product product, int quantity) {
+        if (product.getStock() < quantity) {
+            throw new IllegalStateException("Insufficient stock of " + product.getDescription() + ". Only " + product.getStock() + " currently available.");
+        }
+
         orderItems.add(new OrderItem(product.getId(), quantity));
         product.addHold(quantity);
     }
