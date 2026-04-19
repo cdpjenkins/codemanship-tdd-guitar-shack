c8f95197953fa555e4536c9e99ebb559de2879f6
Chris Jenkins <cdpjenkins@gmail.com>
2026-04-19 20:30:33 +0100

    feat: Removing an item removes the item from the order's list

--- src/test/java/com/codemanship/RemoveItemTest.java
+++ src/test/java/com/codemanship/RemoveItemTest.java
@@ -3,6 +3,7 @@ package com.codemanship;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.ListAssert.assertThatList;
 
 public class RemoveItemTest {
 
@@ -14,5 +15,15 @@ public class RemoveItemTest {
         order.removeItem(product, 1);
 
         assertThat(product.getHold()).isEqualTo(0);
+    }
+
+    @Test
+    void item_is_removed_from_order_item_list() {
+        Product product = new Product(327, "A nice guitar", 1, 1);
+        Order order = new Order();
+
+        order.removeItem(product, 1);
+
+        assertThatList(order.getItems()).isEmpty();
     }
 }
