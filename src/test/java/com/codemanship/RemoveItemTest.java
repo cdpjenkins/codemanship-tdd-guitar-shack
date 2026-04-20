7452a112f873ad774bfe5ec92a67184eb2dd8740
Chris Jenkins <cdpjenkins@gmail.com>
2026-04-20 13:09:22 +0100

    refactor: Refactor tests to make them simpler and better at communicating intent

--- src/test/java/com/codemanship/RemoveItemTest.java
+++ src/test/java/com/codemanship/RemoveItemTest.java
@@ -7,28 +7,36 @@ import static org.assertj.core.api.ListAssert.assertThatList;
 
 public class RemoveItemTest {
 
-    public static final Product PRODUCT = new Product(327, "A nice guitar");
+    public static final int PRODUCT_ID = 327;
+    public static final Product PRODUCT = new Product(PRODUCT_ID, "A nice guitar");
     private final GuitarShack guitarShack = new GuitarShack();
+    private final Order order = new Order(guitarShack);
 
     @Test
     void temporary_hold_is_released_when_item_is_removed() {
-        guitarShack.addStockItem(new StockItem(327, 1, 0));
-        Order order = new Order(guitarShack);
-        order.addItem(PRODUCT, 1);
+        givenStockItem(new StockItem(PRODUCT_ID, 1, 0));
+        givenAnOrderWith(PRODUCT, 1);
 
         order.removeItem(PRODUCT, 1);
 
-        assertThat(guitarShack.getStockItem(327).getHold()).isEqualTo(0);
+        assertThat(guitarShack.getStockItem(PRODUCT_ID).getHold()).isEqualTo(0);
+    }
+
+    private void givenAnOrderWith(Product product, int quantity) {
+        order.addItem(product, quantity);
     }
 
     @Test
     void item_is_removed_from_order_item_list() {
-        guitarShack.addStockItem(new StockItem(327, 1, 0));
-        Order order = new Order(guitarShack);
-        order.addItem(PRODUCT, 1);
+        givenStockItem(new StockItem(PRODUCT_ID, 1, 0));
+        givenAnOrderWith(PRODUCT, 1);
 
         order.removeItem(PRODUCT, 1);
 
         assertThatList(order.getItems()).isEmpty();
+    }
+
+    private void givenStockItem(StockItem stockItem) {
+        guitarShack.addStockItem(stockItem);
     }
 }
