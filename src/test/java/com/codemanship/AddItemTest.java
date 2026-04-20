065f57625509a71cb0e191b51379aa9712a685bf
Chris Jenkins <cdpjenkins@gmail.com>
2026-04-20 07:27:44 +0100

    refactor: Refactor tests to avoid declaring the same product in every test

--- src/test/java/com/codemanship/AddItemTest.java
+++ src/test/java/com/codemanship/AddItemTest.java
@@ -7,49 +7,48 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class AddItemTest {
 
+    public static final Product PRODUCT1 = new Product(327, "Ibanez Tube Screamer");
+    public static final int PRODUCT1_ID = 327;
+
     private final GuitarShack guitarShack = new GuitarShack();
 
     @Test
     void temporary_hold_is_placed_when_sufficient_stock() {
-        Product product = new Product(327, "Ibanez Tube Screamer");
-        guitarShack.addStockItem(new StockItem(327, 7, 0));
+        guitarShack.addStockItem(new StockItem(PRODUCT1_ID, 7, 0));
         Order order = new Order(guitarShack);
 
-        order.addItem(product, 1);
+        order.addItem(PRODUCT1, 1);
 
-        assertThat(guitarShack.getStockItem(327).getHold()).isEqualTo(1);
+        assertThat(guitarShack.getStockItem(PRODUCT1_ID).getHold()).isEqualTo(1);
     }
 
     @Test
     void item_is_added_to_order_list_when_sufficient_stock() {
-        Product product = new Product(327, "Ibanez Tube Screamer");
-        guitarShack.addStockItem(new StockItem(327, 7, 0));
+        guitarShack.addStockItem(new StockItem(PRODUCT1_ID, 7, 0));
         Order order = new Order(guitarShack);
 
-        order.addItem(product, 1);
+        order.addItem(PRODUCT1, 1);
 
-        assertThat(order.getItems()).containsExactly(new OrderItem(327, 1));
+        assertThat(order.getItems()).containsExactly(new OrderItem(PRODUCT1_ID, 1));
     }
 
     @Test
     void raises_error_when_insufficient_product_stock() {
-        Product product = new Product(327, "Ibanez Tube Screamer");
-        guitarShack.addStockItem(new StockItem(327, 1, 0));
+        guitarShack.addStockItem(new StockItem(PRODUCT1_ID, 1, 0));
 
         Order order = new Order(guitarShack);
 
-        assertThatThrownBy(() -> order.addItem(product, 2))
+        assertThatThrownBy(() -> order.addItem(PRODUCT1, 2))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("Insufficient stock of Ibanez Tube Screamer. Only 1 currently available.");
     }
 
     @Test
     void raises_error_when_insufficient_product_stock_available_due_to_a_hold() {
-        Product product = new Product(327, "Ibanez Tube Screamer");
-        guitarShack.addStockItem(new StockItem(327, 1, 0));
+        guitarShack.addStockItem(new StockItem(PRODUCT1_ID, 1, 0));
         Order order = new Order(guitarShack);
 
-        assertThatThrownBy(() -> order.addItem(product, 2))
+        assertThatThrownBy(() -> order.addItem(PRODUCT1, 2))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage("Insufficient stock of Ibanez Tube Screamer. Only 1 currently available.");
     }
--- src/test/java/com/codemanship/RemoveItemTest.java
+++ src/test/java/com/codemanship/RemoveItemTest.java
@@ -7,30 +7,27 @@ import static org.assertj.core.api.ListAssert.assertThatList;
 
 public class RemoveItemTest {
 
+    public static final Product PRODUCT = new Product(327, "A nice guitar");
     private final GuitarShack guitarShack = new GuitarShack();
 
     @Test
     void temporary_hold_is_released_when_item_is_removed() {
-        Product product = new Product(327, "A nice guitar");
         guitarShack.addStockItem(new StockItem(327, 1, 0));
-
         Order order = new Order(guitarShack);
-        order.addItem(product, 1);
+        order.addItem(PRODUCT, 1);
 
-        order.removeItem(product, 1);
+        order.removeItem(PRODUCT, 1);
 
         assertThat(guitarShack.getStockItem(327).getHold()).isEqualTo(0);
     }
 
     @Test
     void item_is_removed_from_order_item_list() {
-        Product product = new Product(327, "A nice guitar");
         guitarShack.addStockItem(new StockItem(327, 1, 0));
-
         Order order = new Order(guitarShack);
-        order.addItem(product, 1);
+        order.addItem(PRODUCT, 1);
 
-        order.removeItem(product, 1);
+        order.removeItem(PRODUCT, 1);
 
         assertThatList(order.getItems()).isEmpty();
     }
